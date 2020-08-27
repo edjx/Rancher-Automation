023649564f195a3a127bfcda56d03c3cda922c9e
sriedjx <69201294+sriedjx@users.noreply.github.com>
2020-08-28 01:38:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ context=c-6c6cm:p-djpmf
 url=https://devcontrol.edjx.network/v3 
 
 rancher login --token $token --context $context $url
-rancher context current
+#rancher context current
 echo "rancher executes"
