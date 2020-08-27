81ec0c2ea208330cf4086903e569ebd5beb9b522
sriedjx <69201294+sriedjx@users.noreply.github.com>
2020-08-28 02:15:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-token=$RANCHER_TOKEN
+token=token-8vvsk:j4bjbfh7xmhhkmrsdszml5k2sknm6g4xhrh89vhl74njn7t55dl7pm
 context=c-6c6cm:p-djpmf
-url=https://devcontrol.edjx.network/v3 
+url=https://devcontrol.edjx.network/v3
 
 rancher login --token $token --context $context $url
 rancher $*
