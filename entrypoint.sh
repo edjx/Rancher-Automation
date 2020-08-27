828707dd6b164f9da616ec1cb03aae2007de6c96
sriedjx <69201294+sriedjx@users.noreply.github.com>
2020-08-28 02:00:10 +0530

    Rancher-deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-token=token-lbqsh:m5fxt7d56kgt756n9pvlcjhnksbmg46hp9jf8zlmwbsqbxvmztxn2f
+token=$TOKEN
 context=c-6c6cm:p-djpmf
 url=https://devcontrol.edjx.network/v3 
 
 rancher login --token $token --context $context $url
-rancher context $*
-echo "rancher executes"
+rancher $*
+echo "rancher commands executes successfully"
