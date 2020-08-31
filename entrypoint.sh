443d05ee281849b48e11cea9a4ff92703c722b58
sriedjx <69201294+sriedjx@users.noreply.github.com>
2020-08-31 22:55:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-token=token-8vvsk:j4bjbfh7xmhhkmrsdszml5k2sknm6g4xhrh89vhl74njn7t55dl7p
+token=token-8vvsk:j4bjbfh7xmhhkmrsdszml5k2sknm6g4xhrh89vhl74njn7t55dl7pm
 context=c-6c6cm:p-djpmf
 url=https://devcontrol.edjx.network/v3
 
